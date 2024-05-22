c738252d681a19d49e985d2f4674528d9d5016e3
s55ma <andrej@krejzi.si>
2024-05-22 19:56:03 +0200

    Create delayed-start-file_check.sh

---
+++ youtubedl/delayed-start-file_check.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# Define the path to the test file on the Samba share
+TEST_FILE="/app/video/testfile.txt"
+
+# Infinite loop to check for the presence of the test file
+while true; do
+  if [ -f "$TEST_FILE" ]; then
+    echo "[$(date)] Test file found. Starting the service..."
+    exec "$@"
+  else
+    echo "[$(date)] Test file not found. Waiting for 10 seconds..."
+    sleep 10
+  fi
+done
